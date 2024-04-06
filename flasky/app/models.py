80d2f5867b0db4556bfff125534ab2a5b950798b
Carlos Ramirez <carlosramirez3982@gmail.com>
2024-04-06 10:42:47 -0600

    Agregue los Jinja Templates del Login, Token e index

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -1,25 +1,37 @@
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from bson import ObjectId
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app
 from . import db
 from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(id=ObjectId(user_id)).first()
 
 class User(UserMixin, db.Document):
     username = db.StringField(required=True)
     email = db.StringField(required=True, unique=True)
     password_hash = db.StringField(required=True)
+    confimed = db.StringField(db.Boolean)
     
     @property
-    def password(self): # Es un metodo disfrazado del atributo password, por ende cuando se invoque el obj.password arrojara el error
+    def password(self):
         raise AttributeError("password is not readable attribute")
 
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
         
+    def generate_confirmation_token(self, expiration=300):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
     
-@login_manager.user_loader
-def load_user(user_id):
-    return User.objects(id=ObjectId(user_id)).first()
+    
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
--- flasky/config.py
+++ flasky/config.py
@@ -1,18 +1,23 @@
 import os 
 
 class Config: 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Hard to guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Hard to guess')
     REMEMBER_COOKIE_DURATION = int(os.environ.get('REMEMBER_COOKIE_DURATION', 10 * 24 * 3600)) 
-    PYTHONDONTWRITEBYTECODE = 1 
-    
+    MAIL_SERVER = os.environ.get('EMAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[AlertAI]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <alertia2023@gmail.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     
     @staticmethod
     def init_app(app):
         pass
 
 class DevelopmentConfig(Config):
-    DEBUG = True
-    
+    DEBUG = True   
     MONGODB_SETTINGS = {
         'host' : os.environ.get('MONGODB_SETTINGS') or 'localhost',
         'db' : 'development_db',
