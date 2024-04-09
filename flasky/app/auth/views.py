e18e72c787852fa23aa6cd3970e0bd7553b836de
Carlos Ramirez <carlosramirez3982@gmail.com>
2024-04-08 21:26:58 -0600

    Se agrego el CSS y el html de Index, Login, Signup y confirm-token, ademas se hizo el modulo de email, se agrego las funcioner verify a los metodos del signupform, y se hizo los metodos de verify_token y generate_token en el modelo de user

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -1,22 +1,59 @@
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user 
+import random
 from . import auth
 from .forms import LoginForm
 from ..models import User
 
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('auth/signup.html')
+
+@auth.route('/token', methods=['GET', 'POST'])
+def token():
+    return render_template('auth/token.html')
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    
+    
     form = LoginForm()
+    
+    # random phrases with icons
+    phrases_icons = {
+        "Did you know that Alert AI can detect more than 10 different accidents?" : "fa-solid fa-globe info-icon",
+        'The name of our AI is "alertito"' : "fa-solid fa-robot info-icon",
+        'Alertito cannot differentiate between an aggressive bite and a gentle bite' : "fa-solid fa-dog info-icon",
+        'All your data is encrypted'  : "fa-solid fa-shield-halved info-icon",
+        'AlertAI was developed by just two programmers' : "fa-solid fa-user-group info-icon",
+        'AlertAI remains active 24 hours a day' : "fa-solid fa-eye info-icon"
+    }
+    
+    # Take a random value
+    phrase, icon = random.choice(list(phrases_icons.items()))
+    
     if form.validate_on_submit():
+        
         user = User.objects(email=form.email.data).first()
-        if user is not None:
-            login_user(user, form.remember_me.data)
-            next = request.args.get('next')
+        
+        # if user exists and passsword is correct
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data) #Register the user 
+            next = request.args.get('next') # Get where the user try to go 
+            
+            # Protect the user from a possible scam
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
+                
             return redirect(next)
+        
         flash('Invalid username or password')
-    return render_template('auth/login.html', form=form)
+        
+    return render_template('auth/login.html', form=form, phrase=phrase, icon=icon)
+
+
 
 @auth.route('/logout')
 @login_required
--- flasky/app/models.py
+++ flasky/app/models.py
@@ -2,9 +2,12 @@ from flask_login import UserMixin
 from bson import ObjectId
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import current_app
+from flask import current_app, session, flash
+from datetime import datetime, timedelta, timezone
+import hashlib
 from . import db
 from . import login_manager
+import random
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,7 +17,7 @@ class User(UserMixin, db.Document):
     username = db.StringField(required=True)
     email = db.StringField(required=True, unique=True)
     password_hash = db.StringField(required=True)
-    confimed = db.StringField(db.Boolean)
+    confimed = db.BooleanField(default=False)
     
     @property
     def password(self):
@@ -24,13 +27,28 @@ class User(UserMixin, db.Document):
     def password(self, password):
         self.password_hash = generate_password_hash(password)
         
-    def generate_confirmation_token(self, expiration=300):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    def generate_confirmation_token(self):
+        data_token = f'{self.email}{self.password_hash}{self.username}'.encode('utf-8')
+        hash_result = hashlib.sha256(data_token).hexdigest()
+        token = hash_result[:4]
+        num_random = random.randint(100, 999)
+        return token + str(num_random)
+
+
+    def confirm_token(self, token):
+        token_generated_time = session.get(token)
+        if token_generated_time:
+            current_time = datetime.now(timezone.utc)
+            token_timestap = token_timestap.replace(tzinfo=timezone.utc)
+            
+            if current_time > token_timestap + timedelta(minutes=3):
+                flash('The token has expired')
+                return 
+            
+            self.confirmed = True
         
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
-    
     
     def __repr__(self):
         return '<User %r>' % self.username
